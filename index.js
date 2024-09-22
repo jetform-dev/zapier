964c715a17d7b0fd98ac89829d9c3b73464a2f73
Eka Putra <ekaputra07@gmail.com>
2024-09-22 22:47:06 +0800

    added trigger for new user

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const authentication = require('./authentication');
 const newOrderTrigger = require('./triggers/new_order.js');
+const newUserTrigger = require('./triggers/new_user.js');
 
 module.exports = {
   version: require('./package.json').version,
   platformVersion: require('zapier-platform-core').version,
   authentication: authentication,
-  triggers: { [newOrderTrigger.key]: newOrderTrigger },
+  triggers: { 
+    [newOrderTrigger.key]: newOrderTrigger,
+    [newUserTrigger.key]: newUserTrigger,
+  },
   searches: {},
 };
